6a8f37aadf0f2a30a9f56cecf92de48c2cb5970b
jsongo <jsongo@qq.com>
2017-10-16 00:17:38 +0800

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { User } from './user.model';
+
+const JOB_LIST = [
+    {id: 1001, name: '学生'},
+    {id: 1002, name: '教师'},
+    {id: 1003, name: '白领'},
+    {id: 1004, name: '领导'},
+    {id: 1005, name: '老板'},
+];
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+    user = new User();
+    jobs = JOB_LIST;
+
+    constructor() {
+        // 初始化
+        this.user.job = JOB_LIST[0].id;
+    }
+    // 调试输出
+    get diagnostic() {
+        return JSON.stringify(this.user);
+    }
+    onSubmit() {
+        
+    }
+}
